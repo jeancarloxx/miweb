e3e4dc07d75f69062e5ab5087bee9f8b5995849c
Jeancito <jeancarlo.rodrigue01@usa.edu.co>
2024-09-07 21:53:43 -0500

    Commit inicial

---
+++ JavaApplication4/src/archivo1/archivo1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package archivo1;
+
+/**
+ *
+ * @author jeancarlo
+ */
+public class archivo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ JavaApplication4/src/archivo1/archivo2.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package archivo1;
+
+/**
+ *
+ * @author jeancarlo
+ */
+public class archivo2 {
+    int nombre;
+}
