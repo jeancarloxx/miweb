4dec05c9d06e4e1b8d292b93944f53b26ce59abd
Jeancito <jeancarlo.rodrigue01@usa.edu.co>
2024-09-08 12:19:22 -0500

    Añadido

--- JavaApplication4/src/archivo1/archivo1.java
+++ JavaApplication4/src/archivo1/archivo1.java
@@ -1,6 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.archivo1 to edit this template
  */
 package archivo1;
 
@@ -8,8 +8,6 @@ package archivo1;
  *
  * @author jeancarlo
  */
-public class hola {
+public class archivo1 {
     
-    
-    int julian;
 }
